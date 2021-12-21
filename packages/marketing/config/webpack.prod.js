80c00c98f4764b59cf1bded9fc28f79b8a6daf42
Alejandro Vásquez <alejandro.vasquez@aizon.ai>
2021-12-21 19:11:23 +0100

    marketing deployment added

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -6,7 +6,8 @@ const packageJson = require('../package.json');
 const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].[hash].js'
+        filename: '[name].[hash].js',
+        publicPath: '/marketing/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
