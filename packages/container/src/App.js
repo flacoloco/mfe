85f1962825d9a851ae29d56f25d804a77ac914c4
Alejandro Vásquez <alejandro.vasquez@aizon.ai>
2022-01-11 16:17:34 +0100

    updated from resources

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
 import MarketingApp from './components/MarketingApp';
 import AuthApp from './components/AuthApp';
 import Header from './components/Header';
-import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 const generateClassName = createGenerateClassName({
-    productionPrefix: 'co'
+  productionPrefix: 'co',
 });
 
-const routes = (
-    <Switch>
-        <Route path='auth' component={AuthApp} />
-        <Route path='/' component={MarketingApp} />
-    </Switch>
-);
-
 export default () => {
-    return (
-        <BrowserRouter>
-            <StylesProvider generateClassName={generateClassName}> 
-                <div>
-                    <Header />   
-                    {routes}
-                </div>
-            </StylesProvider>
-        </BrowserRouter>
-    );
-}
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/auth" component={AuthApp} />
+            <Route path="/" component={MarketingApp} />
+          </Switch>
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
+};
